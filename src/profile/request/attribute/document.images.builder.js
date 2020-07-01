378f81dd3e2f6d29178563dce68e081010799b72
David Grayston <davidgrayston@googlemail.com>
2020-07-01 15:21:18 +0100

    SDK-1496: Add support for document images

---
+++ src/profile/request/attribute/document.images.builder.js
@@ -0,0 +1,41 @@
+const SandboxDocumentImages = require('./document.images');
+const ImageJpeg = require('yoti/src/data_type/image.jpeg');
+const ImagePng = require('yoti/src/data_type/image.png');
+
+/**
+ * @class SandboxDocumentImagesBuilder
+ */
+class SandboxDocumentImagesBuilder {
+  constructor() {
+    this.images = [];
+  }
+
+  /**
+   * @param {Buffer} content
+   *
+   * @returns {SandboxDocumentImagesBuilder}
+   */
+  withJpegContent(content) {
+    this.images.push(new ImageJpeg(content));
+    return this;
+  }
+
+  /**
+   * @param {Buffer} content
+   *
+   * @returns {SandboxDocumentImagesBuilder}
+   */
+  withPngContent(content) {
+    this.images.push(new ImagePng(content));
+    return this;
+  }
+
+  /**
+   * @returns {SandboxDocumentImages}
+   */
+  build() {
+    return new SandboxDocumentImages(this.images);
+  }
+}
+
+module.exports = SandboxDocumentImagesBuilder;
